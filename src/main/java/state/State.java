907636dbba5c23e3a99ec07b06f5bad64d8dc3f1
y.dubovitsky <y.dubovitsky@gmail.con>
2019-02-08 15:44:15 +0300

    Шаблон Состояние:
    - Нужно все поправить.

---
+++ src/main/java/state/State.java
@@ -0,0 +1,32 @@
+package state;
+
+public class State {
+    // Нет шариков
+    final static int SOLD_OUT = 0;
+
+    // Нет монетки
+    final static int NO_QUARTER = 1;
+    final static int HAS_QUARTER = 2;
+    final static int SOLD = 3;
+
+    // Исходное состояние; Перемен. экз. для хранения состояния.
+    int state = SOLD_OUT;
+
+    // Нужно создать методы для каждого действия
+    public void insertQuarter() {
+        switch (state) {
+            case SOLD_OUT : {
+                System.out.println();
+            }
+            case NO_QUARTER : {
+                System.out.println("Введите монетку");
+            }
+            case HAS_QUARTER : {
+                state =
+            }
+            case SOLD : {
+                System.out.println("Все шарики проданы!");
+            }
+        }
+    }
+}
